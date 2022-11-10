1ca959de37b8c087ce71d3bf89fd29a9cf5c7f69
VictoriaAjala <victoria.o.ajala@gmail.com>
2022-11-10 14:30:19 +0100

    added notch

--- src/Toggle/index.js
+++ src/Toggle/index.js
@@ -2,5 +2,13 @@ import React from 'react';
 import "./style.css";
 
 export default function Toggle() {
-    return <div className='toggle'></div>
+    return <div className='toggle'>
+        <div className='notch'></div>
+        <div>
+            <div className='shape sm'></div>
+            <div className='shape sm'></div>
+            <div className='shape md'></div>
+            <div className='shape lg'></div>
+        </div>
+    </div>
 }
